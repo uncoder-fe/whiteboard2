64d1c0dc3b38011d603636a497b8a1d7610591d0
uncoder <xxx.ooo>
2021-10-18 14:02:51 +0800

    chore: 调整生成图片的边距为0

--- src/util.ts
+++ src/util.ts
@@ -221,7 +221,7 @@ export const genShapePosition = (ops) => {
 export function pointsToBase64(shape) {
 	const { points, height, width, type } = shape;
 	const len = points.length;
-	const padding = 5;
+	const padding = 0;
 	// 这里脱离原shape的坐标，只为了找到原点（0，0），获取偏移量
 	const left = Math.min(...points.map((point) => point[0])) || 0;
 	const top = Math.min(...points.map((point) => point[1])) || 0;
